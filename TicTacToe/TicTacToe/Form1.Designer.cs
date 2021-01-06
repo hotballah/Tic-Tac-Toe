c974e9cf2bd0564c5168940eebc4b9df365a5e9c
hotballah <57772094+hotballah@users.noreply.github.com>
2021-01-06 15:40:52 -0500

    Initialize

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBoxA1 = new System.Windows.Forms.PictureBox();
+            this.picBoxB1 = new System.Windows.Forms.PictureBox();
+            this.picBoxC1 = new System.Windows.Forms.PictureBox();
+            this.picBoxC2 = new System.Windows.Forms.PictureBox();
+            this.picBoxB2 = new System.Windows.Forms.PictureBox();
+            this.picBoxA2 = new System.Windows.Forms.PictureBox();
+            this.picBoxC3 = new System.Windows.Forms.PictureBox();
+            this.picBoxB3 = new System.Windows.Forms.PictureBox();
+            this.picBoxA3 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtP1 = new System.Windows.Forms.TextBox();
+            this.txtP2 = new System.Windows.Forms.TextBox();
+            this.txtDraws = new System.Windows.Forms.TextBox();
+            this.txtP1P = new System.Windows.Forms.TextBox();
+            this.txtP2P = new System.Windows.Forms.TextBox();
+            this.txtDrawsP = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxA1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxB1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxC1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxC2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxB2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxA2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxC3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxB3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxA3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxA1
+            // 
+            this.picBoxA1.BackColor = System.Drawing.Color.Red;
+            this.picBoxA1.Location = new System.Drawing.Point(17, 21);
+            this.picBoxA1.Name = "picBoxA1";
+            this.picBoxA1.Size = new System.Drawing.Size(87, 87);
+            this.picBoxA1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxA1.TabIndex = 0;
+            this.picBoxA1.TabStop = false;
+            this.picBoxA1.Click += new System.EventHandler(this.picBoxA1_Click);
+            // 
+            // picBoxB1
+            // 
+            this.picBoxB1.BackColor = System.Drawing.Color.Red;
+            this.picBoxB1.Location = new System.Drawing.Point(110, 21);
+            this.picBoxB1.Name = "picBoxB1";
+            this.picBoxB1.Size = new System.Drawing.Size(87, 87);
+            this.picBoxB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxB1.TabIndex = 1;
+            this.picBoxB1.TabStop = false;
+            this.picBoxB1.Click += new System.EventHandler(this.picBoxB1_Click);
+            // 
+            // picBoxC1
+            // 
+            this.picBoxC1.BackColor = System.Drawing.Color.Red;
+            this.picBoxC1.Location = new System.Drawing.Point(203, 21);
+            this.picBoxC1.Name = "picBoxC1";
+            this.picBoxC1.Size = new System.Drawing.Size(87, 87);
+            this.picBoxC1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxC1.TabIndex = 2;
+            this.picBoxC1.TabStop = false;
+            this.picBoxC1.Click += new System.EventHandler(this.picBoxC1_Click);
+            // 
+            // picBoxC2
+            // 
+            this.picBoxC2.BackColor = System.Drawing.Color.Red;
+            this.picBoxC2.Location = new System.Drawing.Point(203, 114);
+            this.picBoxC2.Name = "picBoxC2";
+            this.picBoxC2.Size = new System.Drawing.Size(87, 87);
+            this.picBoxC2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxC2.TabIndex = 5;
+            this.picBoxC2.TabStop = false;
+            this.picBoxC2.Click += new System.EventHandler(this.picBoxC2_Click);
+            // 
+            // picBoxB2
+            // 
+            this.picBoxB2.BackColor = System.Drawing.Color.Red;
+            this.picBoxB2.Location = new System.Drawing.Point(110, 114);
+            this.picBoxB2.Name = "picBoxB2";
+            this.picBoxB2.Size = new System.Drawing.Size(87, 87);
+            this.picBoxB2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxB2.TabIndex = 4;
+            this.picBoxB2.TabStop = false;
+            this.picBoxB2.Click += new System.EventHandler(this.picBoxB2_Click);
+            // 
+            // picBoxA2
+            // 
+            this.picBoxA2.BackColor = System.Drawing.Color.Red;
+            this.picBoxA2.Location = new System.Drawing.Point(17, 114);
+            this.picBoxA2.Name = "picBoxA2";
+            this.picBoxA2.Size = new System.Drawing.Size(87, 87);
+            this.picBoxA2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxA2.TabIndex = 3;
+            this.picBoxA2.TabStop = false;
+            this.picBoxA2.Click += new System.EventHandler(this.picBoxA2_Click);
+            // 
+            // picBoxC3
+            // 
+            this.picBoxC3.BackColor = System.Drawing.Color.Red;
+            this.picBoxC3.Location = new System.Drawing.Point(203, 207);
+            this.picBoxC3.Name = "picBoxC3";
+            this.picBoxC3.Size = new System.Drawing.Size(87, 87);
+            this.picBoxC3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxC3.TabIndex = 8;
+            this.picBoxC3.TabStop = false;
+            this.picBoxC3.Click += new System.EventHandler(this.picBoxC3_Click);
+            // 
+            // picBoxB3
+            // 
+            this.picBoxB3.BackColor = System.Drawing.Color.Red;
+            this.picBoxB3.Location = new System.Drawing.Point(110, 207);
+            this.picBoxB3.Name = "picBoxB3";
+            this.picBoxB3.Size = new System.Drawing.Size(87, 87);
+            this.picBoxB3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxB3.TabIndex = 7;
+            this.picBoxB3.TabStop = false;
+            this.picBoxB3.Click += new System.EventHandler(this.picBoxB3_Click);
+            // 
+            // picBoxA3
+            // 
+            this.picBoxA3.BackColor = System.Drawing.Color.Red;
+            this.picBoxA3.Location = new System.Drawing.Point(17, 207);
+            this.picBoxA3.Name = "picBoxA3";
+            this.picBoxA3.Size = new System.Drawing.Size(87, 87);
+            this.picBoxA3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxA3.TabIndex = 6;
+            this.picBoxA3.TabStop = false;
+            this.picBoxA3.Click += new System.EventHandler(this.picBoxA3_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(296, 21);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(193, 86);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtP1
+            // 
+            this.txtP1.Location = new System.Drawing.Point(296, 113);
+            this.txtP1.Name = "txtP1";
+            this.txtP1.Size = new System.Drawing.Size(193, 20);
+            this.txtP1.TabIndex = 10;
+            // 
+            // txtP2
+            // 
+            this.txtP2.Location = new System.Drawing.Point(296, 139);
+            this.txtP2.Name = "txtP2";
+            this.txtP2.Size = new System.Drawing.Size(193, 20);
+            this.txtP2.TabIndex = 11;
+            // 
+            // txtDraws
+            // 
+            this.txtDraws.Location = new System.Drawing.Point(296, 165);
+            this.txtDraws.Name = "txtDraws";
+            this.txtDraws.Size = new System.Drawing.Size(193, 20);
+            this.txtDraws.TabIndex = 12;
+            // 
+            // txtP1P
+            // 
+            this.txtP1P.Location = new System.Drawing.Point(495, 113);
+            this.txtP1P.Name = "txtP1P";
+            this.txtP1P.Size = new System.Drawing.Size(100, 20);
+            this.txtP1P.TabIndex = 13;
+            // 
+            // txtP2P
+            // 
+            this.txtP2P.Location = new System.Drawing.Point(495, 139);
+            this.txtP2P.Name = "txtP2P";
+            this.txtP2P.Size = new System.Drawing.Size(100, 20);
+            this.txtP2P.TabIndex = 14;
+            // 
+            // txtDrawsP
+            // 
+            this.txtDrawsP.Location = new System.Drawing.Point(495, 165);
+            this.txtDrawsP.Name = "txtDrawsP";
+            this.txtDrawsP.Size = new System.Drawing.Size(100, 20);
+            this.txtDrawsP.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDrawsP);
+            this.Controls.Add(this.txtP2P);
+            this.Controls.Add(this.txtP1P);
+            this.Controls.Add(this.txtDraws);
+            this.Controls.Add(this.txtP2);
+            this.Controls.Add(this.txtP1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.picBoxC3);
+            this.Controls.Add(this.picBoxB3);
+            this.Controls.Add(this.picBoxA3);
+            this.Controls.Add(this.picBoxC2);
+            this.Controls.Add(this.picBoxB2);
+            this.Controls.Add(this.picBoxA2);
+            this.Controls.Add(this.picBoxC1);
+            this.Controls.Add(this.picBoxB1);
+            this.Controls.Add(this.picBoxA1);
+            this.Name = "Form1";
+            this.Text = "counter";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxA1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxB1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxC1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxC2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxB2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxA2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxC3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxB3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxA3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxA1;
+        private System.Windows.Forms.PictureBox picBoxB1;
+        private System.Windows.Forms.PictureBox picBoxC1;
+        private System.Windows.Forms.PictureBox picBoxC2;
+        private System.Windows.Forms.PictureBox picBoxB2;
+        private System.Windows.Forms.PictureBox picBoxA2;
+        private System.Windows.Forms.PictureBox picBoxC3;
+        private System.Windows.Forms.PictureBox picBoxB3;
+        private System.Windows.Forms.PictureBox picBoxA3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtP1;
+        private System.Windows.Forms.TextBox txtP2;
+        private System.Windows.Forms.TextBox txtDraws;
+        private System.Windows.Forms.TextBox txtP1P;
+        private System.Windows.Forms.TextBox txtP2P;
+        private System.Windows.Forms.TextBox txtDrawsP;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        
+        short turn = 0;
+        short finish = 1;
+        int move;
+        int moved = 0;
+        bool[] player1 = new bool[9];
+        bool[] player2 = new bool[9];
+        PictureBox[] pic = new PictureBox[9];
+        string filePathP1Win = @"C:\Users\mabdi\Desktop\ICS\TicTacToe Database\P1 Win.txt";
+        string filePathP2Win = @"C:\Users\mabdi\Desktop\ICS\TicTacToe Database\P2 Win.txt";
+        string filePathDraw = @"C:\Users\mabdi\Desktop\ICS\TicTacToe Database\Draw.txt";
+
+
+        string gameRecord = "";
+        short gameCounter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            pic[0] = picBoxA1;
+            pic[1] = picBoxB1;
+            pic[2] = picBoxC1;
+            pic[3] = picBoxA2;
+            pic[4] = picBoxB2;
+            pic[5] = picBoxC2;
+            pic[6] = picBoxA3;
+            pic[7] = picBoxB3;
+            pic[8] = picBoxC3;
+
+        }
+        public void Reset()
+        {
+            gameCounter++;
+            btnStart.Text = "" + gameCounter;
+            
+
+            
+           
+
+            btnStart.Update();
+            gameRecord = "";
+            turn = 0;
+            finish = 1;
+            for (short i = 0; i < player1.Length; i++)
+            {
+                player1[i] = false;
+                player2[i] = false;
+            }
+            for (short i = 0; i < pic.Length; i++)
+            {
+                pic[i].Image = null;
+                pic[i].Enabled = true;
+            }
+            moved++;
+
+
+            
+
+        }
+
+        public void Drawer(PictureBox box, short num)
+        {
+            
+            Image imgX = Image.FromFile(@"C:\Users\mabdi\Pictures\XO.png");
+            Image imgO = Image.FromFile(@"C:\Users\mabdi\Pictures\XO (2).png");
+            gameRecord +=  num + ";";
+            switch (turn)
+            {
+                case 0:
+                    turn++;
+                    box.Image = imgX;
+                    player1[num] = true;
+                    break;
+                case 1:
+                    turn--;
+                    box.Image = imgO;
+                    player2[num] = true;
+                    break;
+            }
+            box.Enabled = false;
+            
+            finish++;
+            if ((player1[0] && player1[1] && player1[2]) || (player1[3] && player1[4] && player1[5]) || (player1[6] && player1[7] && player1[8]) || (player1[0] && player1[3] && player1[6]) || (player1[1] && player1[4] && player1[7]) || (player1[2] && player1[5] && player1[8]) || (player1[0] && player1[4] && player1[8]) || (player1[2] && player1[4] && player1[6]))
+            {
+                //MessageBox.Show("Player 1 Wins!!!", "Winner!!!");
+                if (!CheckGames(gameRecord, filePathP1Win))
+                {
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePathP1Win, append: true))
+                    {
+
+                        file.WriteLine(gameRecord, Environment.NewLine);
+                    }
+                }
+                gameRecord = gameRecord.Remove(gameRecord.Length - 6);
+                if (CheckGames(gameRecord, filePathP2Win))
+                {
+                    string[] games = File.ReadAllLines(filePathP2Win);
+                    games = games.Where(val => !val.Contains(gameRecord)).ToArray();
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePathP2Win))
+                    {
+                        for (int i = 0; i < games.Length; i++)
+                        {
+                            file.WriteLine(games[i]);
+                        }
+                    }
+
+                }
+
+                Reset();
+            }
+            else if ((player2[0] && player2[1] && player2[2]) || (player2[3] && player2[4] && player2[5]) || (player2[6] && player2[7] && player2[8]) || (player2[0] && player2[3] && player2[6]) || (player2[1] && player2[4] && player2[7]) || (player2[2] && player2[5] && player2[8]) || (player2[0] && player2[4] && player2[8]) || (player2[2] && player2[4] && player2[6]))
+            {
+                //MessageBox.Show("Player 2 Wins!!!", "Winner!!!");
+                if (!CheckGames(gameRecord, filePathP2Win))
+                {
+                    
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePathP2Win, append: true))
+                    {
+
+                        file.WriteLine(gameRecord, Environment.NewLine);
+
+                    }
+                }
+                gameRecord = gameRecord.Remove(gameRecord.Length - 6);
+                if (CheckGames(gameRecord, filePathP1Win))
+                {
+                    string[] games = File.ReadAllLines(filePathP1Win);
+                    games = games.Where(val => !val.Contains(gameRecord)).ToArray();
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePathP1Win))
+                    {
+
+                        file.Write("");
+
+                    }
+                    for (int i = 0; i < games.Length; i++)
+                    {
+                        using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePathP1Win, append: true))
+                        {
+
+                            file.WriteLine(games[i], Environment.NewLine);
+
+                        }
+                    }
+                }
+
+                Reset();
+            } else if (finish == 10)
+            {
+                //MessageBox.Show("Tie Game!!!", "It's a Draw!!!");
+                if (!CheckGames(gameRecord, filePathDraw))
+                {
+                    
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePathDraw, append: true))
+                    {
+                        file.WriteLine(gameRecord, Environment.NewLine);
+                    }
+                }
+                gameRecord = gameRecord.Remove(gameRecord.Length - 6);
+                if (CheckGames(gameRecord, filePathP2Win))
+                {
+                    string[] games = File.ReadAllLines(filePathP2Win);
+                    games = games.Where(val => !val.Contains(gameRecord)).ToArray();
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePathP2Win))
+                    {
+
+                        file.Write("");
+
+                    }
+                    for (int i = 0; i < games.Length; i++)
+                    {
+                        using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePathP2Win, append: true))
+                        {
+
+                            file.WriteLine(games[i], Environment.NewLine);
+
+                        }
+                    }
+                }
+                if (CheckGames(gameRecord, filePathP1Win))
+                {
+                    string[] games = File.ReadAllLines(filePathP1Win);
+                    games = games.Where(val => !val.Contains(gameRecord)).ToArray();
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePathP1Win))
+                    {
+
+                        file.Write("");
+
+                    }
+                    for (int i = 0; i < games.Length; i++)
+                    {
+                        using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePathP1Win, append: true))
+                        {
+
+                            file.WriteLine(games[i], Environment.NewLine);
+
+                        }
+                    }
+                }
+                Reset();
+            }
+            //if (moved <= 190)
+            //{
+            //    if (turn == 0)
+            //    {
+            //        Computer1();
+            //    }
+            //    else if (turn == 1)
+            //    {
+            //        Computer2();
+            //    }
+            //}
+
+        }
+        public bool CheckGames(string ThisGame, string file)
+        {
+            string[] games = File.ReadAllLines(file);
+            foreach (string game in games)
+            {
+                if (game.Contains(ThisGame))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /* Commented out Computer 2 for debugging reasons. If you want it to play un comment everything, including the methods */
+        //public void Computer2()
+        //{
+        //    string[] games = File.ReadAllLines(filePathP2Win); // FIle p2 for debugging reasons!! Go back to player 1 for automation
+        //    for (int j = 0; j < games.Length; j++)
+        //    {
+        //        if (games[j].Contains(gameRecord))
+        //        {
+        //            for (int i = 0; i < gameRecord.Length; i += 2)
+        //            {
+        //                if (games[j][i] == gameRecord[i])
+        //                {
+
+        //                    move = short.Parse(games[j][i + 2].ToString());
+        //                    if (pic[move].Enabled == true)
+        //                    {
+        //                        goto here;
+        //                    }
+        //                }
+
+        //            }
+
+        //        }
+        //    }
+        //    if (pic[move].Enabled == false)
+        //    {
+        //        int x = 0;
+
+        //        while (pic[move].Enabled == false)
+        //        {
+        //            Random rnd = new Random();
+        //            x = rnd.Next(0, 9);
+        //            move = x;
+        //        }
+        //    }
+        //    here:
+
+        //    Drawer(pic[move], (short)move);
+        //}
+        public void Computer1()
+        {
+
+            string[] games = File.ReadAllLines(filePathP1Win); 
+            for (int j = 0;j < games.Length; j++)
+            {
+                if (games[j].Contains(gameRecord))
+                {
+                    for (int i = 0; i < gameRecord.Length; i += 2)
+                    {
+                        if (games[j][i] == gameRecord[i])
+                        {
+
+                            move = short.Parse(games[j][i + 2].ToString());
+                            if (pic[move].Enabled == true)
+                            {
+                                goto here;
+                            }
+                        }
+
+                    }
+
+                }
+            }
+            if (pic[move].Enabled == false)
+            {
+                int x = 0;
+
+                while (pic[move].Enabled == false)
+                {
+                    Random rnd = new Random();
+                    x = rnd.Next(0, 9);
+                    move = x;
+                }
+            }
+            here:
+
+            Drawer(pic[move], (short)move);
+
+
+
+        }
+        private void picBoxC3_Click(object sender, EventArgs e)
+        {
+            move = 8;
+
+            Drawer(pic[8], 8);
+
+            Computer1();
+
+        }
+
+        private void picBoxB3_Click(object sender, EventArgs e)
+        {
+            move = 7;
+            Drawer(pic[7], 7);
+            Computer1();
+
+        }
+
+        private void picBoxA3_Click(object sender, EventArgs e)
+        {
+            move = 6;
+            Drawer(pic[6], 6);
+            Computer1();
+
+        }
+
+        private void picBoxC2_Click(object sender, EventArgs e)
+        {
+            move = 5;
+            Drawer(pic[5], 5);
+            Computer1();
+
+        }
+
+        private void picBoxB2_Click(object sender, EventArgs e)
+        {
+            move = 4;
+            Drawer(pic[4], 4);
+            Computer1();
+
+        }
+
+        private void picBoxA2_Click(object sender, EventArgs e)
+        {
+            move = 3;
+            Drawer(pic[3], 3);
+            Computer1();
+
+        }
+
+        
+
+        private void picBoxC1_Click(object sender, EventArgs e)
+        {
+            move = 2;
+            Drawer(pic[2], 2);
+            Computer1();
+
+        }
+
+        private void picBoxB1_Click(object sender, EventArgs e)
+        {
+            move = 1;
+            Drawer(pic[1], 1);
+            Computer1();
+
+        }
+
+        private void picBoxA1_Click(object sender, EventArgs e)
+        {
+            move = 0;
+            Drawer(pic[0], 0);
+            Computer1();
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+            Computer1();
+            gameCounter = 0;
+            moved = 0;
+        }
+    }
+}
